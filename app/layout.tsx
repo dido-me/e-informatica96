4e6ea065f3cff420a7c71c36b3e6553f61cff025
didojs <efvillantoyt@ucvvirtual.edu.pe>
2023-06-06 00:51:16 -0500

    feat: darkmode blue color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { ReactNode } from 'react'
-
 import './globals.css'
+import type { ReactNode } from 'react'
 import { Navbar } from './components'
 
 export const metadata = {
   title:
     'Informartico96 - Venta de Servidores, Computadoras, Laptops, Impresoras, Accesorios y más',
-  description: 'Computadora Firewall Blade Servidor Storage Switch Cisco DELL EMC Fortinet 3PAR HP IBM Lenovo Huawei Juniper Oracle #Cisco #HP #DELL #Lenovo #Switch #Servidor #Server #AccesPoint #Transceiver Google #Google'
+  description:
+    'Computadora Firewall Blade Servidor Storage Switch Cisco DELL EMC Fortinet 3PAR HP IBM Lenovo Huawei Juniper Oracle #Cisco #HP #DELL #Lenovo #Switch #Servidor #Server #AccesPoint #Transceiver Google #Google'
 }
 
 export default function RootLayout ({ children }: { children: ReactNode }) {
   return (
     <html lang='es'>
-      <body>
+      <body className='dark:bg-[#0A101A]'>
         <Navbar />
         {children}
       </body>
