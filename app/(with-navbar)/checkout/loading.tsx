f74f3108f5ed191626df3509dfa227d76aea82fb
didojs <efvillantoyt@ucvvirtual.edu.pe>
2023-10-20 00:44:07 -0500

    fix: loading checkout

---
+++ app/(with-navbar)/checkout/loading.tsx
@@ -0,0 +1,8 @@
+export default function CheckoutLoading () {
+  return (
+    <main className='flex items-center justify-center w-11/12 pt-16 m-auto'>
+      <span className='loading loading-spinner loading-lg' />
+      <h1>Cargando...</h1>
+    </main>
+  )
+}
